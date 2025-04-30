fb08e948aed154ee78256b5febdc0babd0195641
Kalle <kalle@kandru.de>
2025-05-01 00:34:54 +0200

    bump version to 0.0.4

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace CardboardPlayer
 {
     public partial class CardboardPlayer
     {
-        public override string ModuleVersion => "0.0.3";
+        public override string ModuleVersion => "0.0.4";
     }
 }
