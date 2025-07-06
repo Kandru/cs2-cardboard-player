981d98e03c1dadef1f0ff55d362b1cd486e7ac9e
Kalle <kalle@kandru.de>
2025-07-06 17:30:23 +0200

    fix: precache

--- src/Precache.cs
+++ src/Precache.cs
@@ -10,6 +10,7 @@ namespace CardboardPlayer
             "models/cs2/cardboard/dummy_ct_2.vmdl",
             "models/cs2/cardboard/dummy_ct_3.vmdl",
             "models/cs2/cardboard/dummy_ct_4.vmdl",
+            "models/cs2/cardboard/dummy_t_1.vmdl",
             "models/cs2/cardboard/dummy_t_2.vmdl",
             "models/cs2/cardboard/dummy_t_3.vmdl",
             "models/cs2/cardboard/dummy_t_4.vmdl",
