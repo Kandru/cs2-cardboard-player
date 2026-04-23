b7943a8d5c527f0ff984a5af082cb1ad4958646f
derkalle4 <kalle@kandru.de>
2026-04-23 15:50:35 +0200

    bump version to 26.04.1

--- src/Version.cs
+++ src/Version.cs
@@ -2,6 +2,6 @@ namespace CardboardPlayer
 {
     public partial class CardboardPlayer
     {
-        public override string ModuleVersion => "0.0.10";
+        public override string ModuleVersion => "26.04.1";
     }
 }
